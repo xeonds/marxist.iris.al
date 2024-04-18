92bad335c7f1a1cae046dbf9752a10e979167b66
xeonds <xeonds@stu.xidian.edu.cn>
2024-04-18 11:19:46 +0800

    update script: supports extensively sync

--- mirror.sh
+++ mirror.sh
@@ -4,6 +4,21 @@ SOURCE_URL="https://www.marxists.org/chinese/index.html"
 TARGET_DIR="mirror"
 
 mkdir -p $TARGET_DIR
+touch mirror/www.marxists.org/chinese/index.html
 
-wget --mirror --convert-links --adjust-extension --page-requisites --no-parent $SOURCE_URL -P$TARGET_DIR
-
+wget --recursive \
+    --page-requisites \
+    --mirror \
+    --no-check-certificate \
+    --convert-links \
+    --adjust-extension \
+    --no-parent \
+    --random-wait \
+    --continue \
+    -L \
+    -e robots=off \
+    --user-agent="Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/72.0.3626.121 Safari/537.36" \
+    --timestamping \
+    --span-hosts \
+    $SOURCE_URL \
+    -P $TARGET_DIR
